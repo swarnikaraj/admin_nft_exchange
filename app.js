fd91b44971883741931cfbc2f031b197a6e50e2d
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2022-08-15 15:44:19 +0530

    updated

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const fs = require("fs");
 
 const { register, login } = require("./src/controller/Auth/auth.controller");
 
-
 // this controller has post and delete request for collection
 // it is Admin controller for collection
 // it uses opensea api by default
@@ -19,17 +18,15 @@ const AdminCollectionController = require("./src/controller/admin.collection.con
 // it fetches data from data base
 const CollectionController = require("./src/controller/collection.controller");
 
-
 // it is admin controller for NFT
 // it uses ALCHEMY api by default
-// it Doesnt append the cloud image to the object 
+// it Doesnt append the cloud image to the object
 // it has post and delete request for nfts
 const AdminNftController = require("./src/controller/admin.nft.controller");
 
-
 // it is client controller for NFT
-// it Doesnt append the cloud image to the object 
-// it has get request for nfts 
+// it Doesnt append the cloud image to the object
+// it has get request for nfts
 const NftController = require("./src/controller/admin.nft.controller");
 
 // this controller will update token Uri for the nfts using blockchain middleware .
@@ -50,13 +47,11 @@ const NftDefectedFilterController = require("./src/controller/defectedNft.contro
 
 // this controller will find single nft data using addres and index
 // this controller uses moralis node
-const SingleNftController=require("./src/controller/single.nft.controller")
+const SingleNftController = require("./src/controller/single.nft.controller");
 
+const HistoryController = require("./src/controller/nftHistory.controller");
 
-const HistoryController=require("./src/controller/nftHistory.controller")
-
-
-const CollectionSearchController=require("./src/controller/searchCollection.controller")
+const CollectionSearchController = require("./src/controller/searchCollection.controller");
 
 const app = express();
 
@@ -64,7 +59,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Nft Exchange Admin panel");
   res.end();
 });
-
 
 app.use(express.json());
 
@@ -90,60 +84,51 @@ cloudinary.config({
 // this controller has post and delete request for collection
 // it can only be operated via admin fronend
 // it only takes address as params
-app.use("admin/collection", AdminCollectionController);
+app.use("/admin/collection", AdminCollectionController);
 
 //non-authenticated and open for client
 // this controller has get request for collection
 // it takes /:address in the path to get single collection
-app.use("collection",CollectionController)
+app.use("/collection", CollectionController);
 
 // returns single nft data
 // It has only get request and open for client
 // it takes /:address/:index in the path to get single nft
-app.use("collection/asset",SingleNftController);
+app.use("/collection/asset", SingleNftController);
 
 // returns nfts array for a collection address
 // It has only get request and open for client
 // it takes /:address in the path to get list of nfts corresponding to a address
-app.use("collection/bundle",NftController)
+app.use("/collection/bundle", NftController);
 
 // return nft history
 // It has only get request and open for client
 // it takes /:address/:index in the path to get single nft
-app.use("collection/history/",HistoryController);
-
+app.use("/collection/history/", HistoryController);
 
 // it has get post delete request for Nft
 // it only takes address as params
-app.use("admin/nft", AdminNftController);
-
-
-
+app.use("/admin/nft", AdminNftController);
 
 // it takes updateDefected/tokenURI in the path for tokenURI update
 // it takes defected array of nfts as req.body
-app.use("admin/nft/blockChainUpdate", NftBlockchainUpdateController);
+app.use("/admin/nft/blockChainUpdate", NftBlockchainUpdateController);
 
 // it takes /updateDefected in the path
 // it takes defected array of nfts as req.body
-app.use("admin/nft/alchemyUpdate", NftAlchemyUpdateController);
-
+app.use("/admin/nft/alchemyUpdate", NftAlchemyUpdateController);
 
 // it takes "updateDefected/:address in the path
-app.use("admin/nft/cloudImageUpdate", NftCloudImageUpdateController);
-
+app.use("/admin/nft/cloudImageUpdate", NftCloudImageUpdateController);
 
 // it takes /:address in the path
-app.use("admin/nft/filter/defected", NftDefectedFilterController);
-
+app.use("/admin/nft/filter/defected", NftDefectedFilterController);
 
 // it results search in collection by address and by name
 // use address/:address for address search
-// this is unauthenticated 
+// this is unauthenticated
 // use /:name for name search
-app.use("collection/search", CollectionSearchController);
-
-
+app.use("/collection/search", CollectionSearchController);
 
 app.listen(port, async () => {
   await connect();
